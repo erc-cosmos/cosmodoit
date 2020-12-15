b8af08069b341bea0a8ea31600507fc72f6c4f6c
Daniel_Bedoya <daniel.bedoya.r@gmail.com>
2020-12-15 14:37:50 +0100

    updated function to export csv

--- music_features/get_loudness.m
+++ music_features/get_loudness.m
@@ -1,4 +1,4 @@
-function [L] = get_loudness(audio_file, plot_loudness)
+function [L] = get_loudness(audio_file, plot_loudness, export_loudness)
 % Get loudness
 %  [L] = get_loudness(audio_file, plot_loudness)
 %  audio_file    : path to an audio file
@@ -7,6 +7,9 @@ function [L] = get_loudness(audio_file, plot_loudness)
 
 if nargin < 2 % Plot by default
     plot_loudness = true;
+end
+if nargin < 3 % Export by default
+    export_loudness = true;
 end
 
 % Read audio
@@ -28,6 +31,34 @@ if plot_loudness
     plot(L(:,1), L(:,3), 'LineStyle', '-.', 'LineWidth', 0.2, 'Color', [255 160 0]/255)
     plot(L(:,1), L(:,4), 'LineStyle', '-',  'LineWidth', 3.8, 'Color', [139 0   0]/255)
     ylabel('Normalized Loudness (sone)')
+    xlim([L(1,1) L(end,1)])
+    ylim([0 1])
     legend('original', 'normalized', 'smoothed')
 end
+
+% Export
+if export_loudness
+    [fpath, fname, ~] = fileparts(audio_file);
+    N                 = length(L);
+    varNames          = {'Time', 'Loudness', 'Loudness_norm', 'Loudness_smooth'};
+    varTypes          = {'double','double', 'double', 'double'};
+    T                 = table('Size',[N 4],'VariableTypes', varTypes, 'VariableNames', varNames);
+    T.Time            = L(:,1);
+    T.Loudness        = L(:,2);
+    T.Loudness_norm   = L(:,3);
+    T.Loudness_smooth = L(:,4);
+    % Save table
+    table_name        = strcat(fname, '_loudness','.csv');
+    table_exp         = fullfile(fpath, table_name);
+    writetable(T, table_exp)
+    if isfile(table_exp)
+        disp(['Exported: ' table_exp])
+    end
+end
+
+% Clear L if there are no output arguments
+if nargout==0
+   clearvars L
+end
+
 end
