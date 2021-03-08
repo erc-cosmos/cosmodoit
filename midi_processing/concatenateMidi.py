0e199e06b42a253d7e6f0cb87b698f80bac3e4ea
Corentin Guichaoua <guichaoua@math.unistra.fr>
2021-03-08 17:00:41 +0100

    Cleanup on latest scripts

--- midi_processing/concatenateMidi.py
+++ midi_processing/concatenateMidi.py
@@ -5,47 +5,54 @@ import mido
 import argparse
 
 def concatMidi(infilenames, outfilename, outmetafile=None, padding=5):
+    """
+    Concatenates the input midi files into a large one
+    infilenames -- iterable of paths containing the files to concatenate
+    outfilename -- path to output file
+    outmetafile -- path to the output meta-descriptor file (optional)
+    padding -- gap, in seconds, between files (default=5)
+    """
     ppq = mido.MidiFile(infilenames[0]).ticks_per_beat
-    newMid = mido.MidiFile(type=1,ticks_per_beat = ppq)
-
+    
     offset = 0
     padNext = False
     mainTrack = mido.MidiTrack()
-    lastTempoTick = 0
-    lastTempoTime = 0
     meta = []
     # Default value for tempo; might be set by a value for set_tempo later
     tempo = 500000
 
     for infilename in infilenames:
         midi = mido.MidiFile(infilename)
-
-        # Use default for pulses per quarter note if its not set
+        # Warning if PPQ changes
         if midi.ticks_per_beat and (midi.ticks_per_beat != ppq):
             print("PPQ mismatch", ppq, midi.ticks_per_beat)
-
+        # Concatenate events
         for event in mido.merge_tracks(midi.tracks):
             if padNext:
                 padNext = False
                 event.time += round(mido.second2tick(padding,ppq,tempo))
             if event.type == 'set_tempo':
                 tempo = event.tempo
-            if not isinstance(event.time, int):
-                print(event)
             mainTrack.append(event)
-
+        # Meta information
         duration = midi.length
         meta.append((infilename, offset, offset + duration))
         offset += duration + padding
+        # Setup next iteration
         padNext=True
 
+    newMid = mido.MidiFile(type=1,ticks_per_beat = ppq)
     newMid.tracks = [mainTrack]
     newMid.save(outfilename)
+    
     if outmetafile is not None:
-        with open(outmetafile,'w') as metafile:
-            writer = csv.writer(metafile)
-            writer.writerow(("File","Start","End"))
-            writer.writerows(meta)
+        writeCsv(metafile, meta, ("File", "Start", "End") )
+
+def writeCsv(outputFile, data, header):
+    with open(outmetafile,'w') as outputFile:
+        writer = csv.writer(outputFile)
+        writer.writerow(header)
+        writer.writerows(meta)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- midi_processing/fixDoubleOnsets.py
+++ midi_processing/fixDoubleOnsets.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
 
-# from get_midiEvents import *
 import csv
 import mido
-
-
-# def findDoubleOnsets():
-#     filename = "music_features/test_midi/Variations/Var I.mid"
-
-#     midiNotes = [note for note in get_midiEvents(filename) if note["Type"]=='note_on']
-
-#     doubles = []
-#     threshold = 0.005 #10ms
-#     for note1 in midiNotes:
-#         for note2 in midiNotes:
-#             if note1['Note']==note2['Note'] and note2["StartTime"] > note1["StartTime"] and (note1["EndTime"] + threshold) > note2["StartTime"]:
-#                 doubles.append((note1,note2))
-
-#     with open("test.csv", mode='w') as csvFile:
-#             writer = csv.writer(csvFile)
-#             for note1,note2 in doubles:
-#                 writer.writerow([note2['StartTime']])
 
 def isNoteOff(message):
     return message.type == 'note_off' or (message.type =='note_on' and message.velocity == 0)
 
-def fixDoubleOnsets(infilename, outfilename, threshold=.005, verbose=False):
+def isDoubleOnset(threshold, tempo, ppq, event, nextEvent):
+    return (isNoteOff(event) and nextEvent.type == "note_on" and event.note == nextEvent.note 
+        and mido.tick2second(nextEvent.time, ppq, tempo) < threshold)
+
+def fixDoubleOnsets(infilename, outfilename, threshold=.005):
     midi = mido.MidiFile(infilename)
-    newMid = mido.MidiFile()
     
     # Default value for tempo; might be set by a value for set_tempo later
     tempo = 500000
-
     # Use default for pulses per quarter note if its not set
     if midi.ticks_per_beat:
         ppq = midi.ticks_per_beat
@@ -48,20 +31,17 @@ def fixDoubleOnsets(infilename, outfilename, threshold=.005, verbose=False):
 
         carryTime = 0
         for event,nextEvent in zip(track,itNext):
-            if skipNext:
+            if skipNext: # Spurious onset
                 skipNext = False
                 carryTime += event.time
                 continue
-            elif isNoteOff(event) and nextEvent.type == "note_on" and event.note == nextEvent.note and mido.tick2second(nextEvent.time, ppq, tempo) < threshold:
+            elif isDoubleOnset(threshold, tempo, ppq, event, nextEvent): # Spurious release
                 skipNext = True
-                carryTime = event.time
+                carryTime += event.time
                 continue
             else:
                 if event.type == 'set_tempo':
                     tempo = event.tempo
-                    if verbose:
-                        print('    Tempo: ' + str(tempo))
-                        print('    BPM: ' + str(mido.tempo2bpm(tempo)))    
                 event.time += carryTime
                 carryTime = 0
                 newTrack.append(event)
@@ -73,7 +53,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--inFile')
     parser.add_argument('--outFile')
+    parser.add_argument('--threshold', default=0.005, type=float)
     args = parser.parse_args()
     
-    fixDoubleOnsets(args.inFile, args.outFile)
+    fixDoubleOnsets(args.inFile, args.outFile, args.threshold)
     
