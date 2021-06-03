5e21d6c203c54e85e7d3e00c37a0e8869ab84429
Corentin Guichaoua <guichaoua@math.unistra.fr>
2021-06-03 18:49:57 +0200

    Added extra test file set

--- tests/test_beats.py
+++ tests/test_beats.py
@@ -1,11 +1,13 @@
 import get_beats
 import get_alignment
+import pytest
 
 
-def test_sorted_beats():
-    ref='tests/test_data/Chopin_Ballade_No2_ref.mscz'
-    perf='tests/test_data/Chopin_Ballade_No2_perf.mid'
+test_files = [("tests/test_data/Chopin_Ballade_No2_ref.mscz","tests/test_data/Chopin_Ballade_No2_perf.mid"),
+              ("tests/test_data/Fur_Elise_ref.mscz","tests/test_data/Fur_Elise_perf.mid")]
 
+@pytest.mark.parametrize("ref, perf", test_files)
+def test_sorted_beats(ref, perf):
     alignment = get_alignment.get_alignment(refFilename=ref, perfFilename=perf, cleanup=False, midi2midiExecLocation="music_features/MIDIToMIDIAlign.sh")
 
     beats = get_beats.get_beats(alignment, plotting=False, guess=True)
