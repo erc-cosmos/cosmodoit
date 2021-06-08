74db17159da830078bb63dbe0afb457e07e00a33
Daniel_Bedoya <daniel.bedoya.r@gmail.com>
2021-06-08 12:02:14 +0200

    Tempo is exported with original beat timestamps plus optional midpoint timestamps. _all suffix added for default use

--- music_features/get_tempo.py
+++ music_features/get_tempo.py
@@ -6,61 +6,74 @@ import pandas as pd
 
 def importBeats(inputPath):
     """ Import annotated csv file. Must include a header named 'beats' """
-    df      = pd.read_csv(inputPath)
-    beats_v = df['beats'].to_numpy()
-    return beats_v
+    df       = pd.read_csv(inputPath)
+    beatTime = df['beats'].to_numpy()
+    return beatTime
 
-def genExportName(inputPath):
+def genExportName(inputPath, columns):
     """ Generate export file name from input.
     Beat annotation files must include _beats before extension"""
     baseName    = os.path.basename(inputPath)
     baseName, _ = os.path.splitext(baseName)
     baseName, _ = baseName.split('_beats')
     dirName     = os.path.dirname(inputPath)
-    exportName  = os.path.join(dirName, baseName + '_tempo.csv')
+    if columns.lower() == 'all':
+        fileSuffix  = '_tempo_all'
+    else:
+        fileSuffix  = '_tempo'
+    exportName  = os.path.join(dirName, baseName + fileSuffix + '.csv')
     return exportName
 
-def computeTempo(beats_v):
+def computeTempo(beatTime):
     """ Compute tempo from beat onsets """
-    N         = len(beats_v)
-    beatCount = np.linspace(1,N-1,N-1, dtype='int') # beat count starting from 1
-    beatTime  = (beats_v[0:N-1] + beats_v[1:N])/2   # time from midpoint between two bars
-    tempo_v   = 60/(beats_v[1:N]-beats_v[0:N-1])    # tempo vector
-    return [beatCount, beatTime, tempo_v]
+    N            = len(beatTime)
+    beatCount    = np.linspace(0, N-1, N, dtype='int')  # beat count starting from 0
+    beatMidpoint =    (beatTime[1:N] + beatTime[0:N-1])/2 # time from midpoint between two bars
+    tempo_v      = 60/(beatTime[1:N] - beatTime[0:N-1])   # tempo vector
+    beatMidpoint = np.append(np.nan, beatMidpoint)      # account for first sample
+    tempo_v      = np.append(np.nan, tempo_v)           # account for first sample
+    return [beatCount, beatMidpoint, tempo_v]
 
-def makeTempoTable(beats_v):
+def makeTempoTable(beatTime):
     """ Create a pandas DataFrame from tempo computation """
-    [beatCount, beatTime, tempo_v] = computeTempo(beats_v)
-    data  = {'Beats':beatCount, 'Time':beatTime, 'Tempo':tempo_v}
+    [beatCount, beatMidpoint, tempo_v] = computeTempo(beatTime)
+    data  = {'Count':beatCount, 'Time':beatTime, 'beatMidpoint':beatMidpoint, 'Tempo':tempo_v}
     tempo = pd.DataFrame.from_dict(data)
     return tempo
+
+def plotTempoCurve(tempo):
+    """ Plot tempo curve over time """
+    import matplotlib.pyplot as plt
+    plt.plot(tempo['Time'], tempo['Tempo'], c='black')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Tempo [BPM]')
+    plt.grid()
+    plt.show()
 
 def getTempo(inputPath, plotTempo, exportTempo, columns):
     """
     Import beat annotations and compute tempo table with beat counts
     Input
-        beats_v    : 1D numpy array
+        beatTime   : 1D numpy array
         plotTempo  : bool, default False
         exportTempo: bool, default True
-        columns    : str, default 'all' ('all', 'time', 'beats')
+        columns    : str, default 'all' ('all', 'time', 'beats', 'midpoint')
     Output
         tempo      : pandas DataFrame with selected columns
     """
-    beats_v = importBeats(inputPath)
-    tempo   = makeTempoTable(beats_v)
+    beatTime = importBeats(inputPath)
+    tempo    = makeTempoTable(beatTime)
     if plotTempo:
-        import matplotlib.pyplot as plt
-        plt.plot(tempo['Time'], tempo['Tempo'], c='black')
-        plt.xlabel('Time [s]')
-        plt.ylabel('Tempo [BPM]')
-        plt.grid()
-        plt.show()
+        plotTempoCurve(tempo)
     if columns.lower() == 'time':
-        tempo.drop('Beats', axis=1, inplace=True)
+        tempo.drop(['Count', 'beatMidpoint'], axis=1, inplace=True)
     if columns.lower() == 'beats':
-        tempo.drop('Time', axis=1, inplace=True)
+        tempo.drop(['Time', 'beatMidpoint'], axis=1, inplace=True)
+    if columns.lower() == 'midpoint':
+        tempo.drop(['Count', 'Time'], axis=1, inplace=True)
+        tempo.drop(tempo.index[0], inplace=True)
     if exportTempo:
-        exportName = genExportName(inputPath)
+        exportName = genExportName(inputPath, columns)
         tempo.to_csv(exportName, sep=',', index=False)
         print('Exported to: {}'.format(exportName))
     return tempo
