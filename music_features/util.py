239e06cbf21c6293aa7728641d0b52153d6a1662
Corentin Guichaoua <guichaoua@math.unistra.fr>
2021-07-08 10:40:52 +0200

    Refactor for file paths

--- music_features/util.py
+++ music_features/util.py
@@ -1,4 +1,6 @@
 import csv
+import os
+import functools
 
 def string_escape_concat(strings, sep=' '):
     """Concatenate strings after wrapping them in quotes."""
@@ -19,3 +21,14 @@ def write_file(filename,data):
         writer = csv.DictWriter(csvFile, fieldnames=fields)
         writer.writeheader()
         writer.writerows(data)
+
+
+def generate_target_path(original_path, working_folder, extension):
+    """Generate a target path."""
+    path_noext, _ = os.path.splitext(os.path.basename(original_path))
+    return os.path.join(working_folder, path_noext + extension)
+
+
+def targets_factory(original_path, working_folder):
+    """Create a target path factory for a given original and working folder."""
+    return functools.partial(generate_target_path, original_path=original_path, working_folder=working_folder)
