cda6e815f5309bd9df0fa0c64eae4d0e5d68db7b
Corentin Guichaoua <guichaoua@math.unistra.fr>
2021-07-08 16:41:45 +0200

    Updated test_beats to match API change

--- tests/test_beats.py
+++ tests/test_beats.py
@@ -1,4 +1,5 @@
 import get_beats
+import get_alignment
 import pytest
 import os
 
@@ -14,7 +15,7 @@ def test_files():
 
 @pytest.mark.parametrize("ref, perf", test_files())
 def test_sorted_beats(ref, perf):
-    alignment = get_beats.get_alignment(refFilename=ref, perfFilename=perf, cleanup=False, midi2midiExecLocation="music_features/MIDIToMIDIAlign.sh")
+    alignment = get_alignment.get_alignment(refFilename=ref, perfFilename=perf, cleanup=False)
     reference_beats = get_beats.make_beat_reference(alignment, guess=True)
 
     beats = get_beats.get_beats(alignment, reference_beats=reference_beats)
@@ -23,7 +24,7 @@ def test_sorted_beats(ref, perf):
 
 @pytest.mark.parametrize("ref, perf", test_files())
 def test_sorted_ref_beats_manual(ref, perf):
-    alignment = get_beats.get_alignment(refFilename=ref, perfFilename=perf, cleanup=False, midi2midiExecLocation="music_features/MIDIToMIDIAlign.sh")
+    alignment = get_alignment.get_alignment(refFilename=ref, perfFilename=perf, cleanup=False)
     reference_beats = get_beats.make_beat_reference(alignment, guess=True)
 
     sorted(reference_beats) == reference_beats
@@ -31,7 +32,7 @@ def test_sorted_ref_beats_manual(ref, perf):
 
 @pytest.mark.parametrize("ref, perf", test_files())
 def test_sorted_ref_beats_prettymidi(ref, perf):
-    alignment = get_beats.get_alignment(refFilename=ref, perfFilename=perf, cleanup=False, midi2midiExecLocation="music_features/MIDIToMIDIAlign.sh")
+    alignment = get_alignment.get_alignment(refFilename=ref, perfFilename=perf, cleanup=False)
     ref_midi = ref.replace(".mscz", ".mid")
     reference_beats = get_beats.get_beat_reference_pm(ref_midi)
 
@@ -40,7 +41,7 @@ def test_sorted_ref_beats_prettymidi(ref, perf):
 
 @pytest.mark.parametrize("ref, perf", test_files())
 def test_no_outliers(ref, perf):
-    alignment = get_beats.get_alignment(refFilename=ref, perfFilename=perf, cleanup=False, midi2midiExecLocation="music_features/MIDIToMIDIAlign.sh")
+    alignment = get_alignment.get_alignment(refFilename=ref, perfFilename=perf, cleanup=False)
     reference_beats = get_beats.make_beat_reference(alignment, guess=True)
 
     beats = get_beats.get_beats(alignment, reference_beats=reference_beats)
