2735ea968fbdeb3459e04fdbb84e18fa57e42cb5
Corentin Guichaoua <guichaoua@math.unistra.fr>
2021-09-01 17:02:49 +0200

    Filter which tasks to run based on found files

--- dodo.py
+++ dodo.py
@@ -1,25 +1,25 @@
-from genericpath import isdir
 import sys
-import warnings
 import os
-
 sys.path.append(os.path.join(os.path.dirname(__file__), "music_features"))
-import get_loudness
-import get_alignment
-import get_sustain
-import get_beats
-import get_onset_velocity
-import get_tension
-import argparse
-import doit
 
-from util import run_doit
+import doit
+import argparse
+import get_tension
+import get_onset_velocity
+import get_beats
+import get_sustain
+import get_alignment
+import get_loudness
+from genericpath import isdir
+import warnings
+
 
 
 DOIT_CONFIG = {'action_string_formatting': 'both'}
 
 # default_working_folder = os.path.join(doit.get_initial_workdir(), 'tmp')
 default_working_folder = 'tmp'
+
 
 def discover_files_by_type(base_folder="tests/test_data"):
     """Find targets in a feature-type first directory structure."""
@@ -30,8 +30,8 @@ def discover_files_by_type(base_folder="tests/test_data"):
              for f in sorted(os.listdir(os.path.join(base_folder, "perfs")))
              if '.mid' in f]
     wavs = [os.path.join(base_folder, "perfs", f)
-             for f in sorted(os.listdir(os.path.join(base_folder, "perfs")))
-             if '.wav' in f]
+            for f in sorted(os.listdir(os.path.join(base_folder, "perfs")))
+            if '.wav' in f]
     assert len(scores) == len(perfs)
     assert len(scores) == len(wavs)
     piece_ids = (os.path.splitext(perf)[0] for perf in perfs)
@@ -40,29 +40,30 @@ def discover_files_by_type(base_folder="tests/test_data"):
 
 def discover_files_by_piece(base_folder='tests/test_data/piece_directory_structure'):
     """Find targets in a piece first directory structure.
-    
+
     This expects pieces to be in one folder each"""
 
     if doit.get_initial_workdir() != os.getcwd():
         base_folder = os.getcwd()
-    piece_folders = [os.path.join(base_folder, folder) 
-        for folder in os.listdir(base_folder) 
-        if os.path.isdir(os.path.join(base_folder, folder)) and folder != 'tmp']
+    piece_folders = [os.path.join(base_folder, folder)
+                     for folder in os.listdir(base_folder)
+                     if os.path.isdir(os.path.join(base_folder, folder)) and folder != 'tmp']
+
     def find_ext(path, ext):
-        files = [os.path.join(path,f) for f in os.listdir(path) if ext in f]
+        files = [os.path.join(path, f) for f in os.listdir(path) if ext in f]
         if len(files) == 0:
             warnings.warn(f"Found no file with extension {ext} in {path}")
             return None
         if len(files) > 1:
             warnings.warn(f"Found more than one file matching extension {ext} in {path} (using {files[0]})")
         return files[0]
-    grouped_files = [(folder, *(find_ext(folder, ext) for ext in ('.mscz', '.mid', '.wav'))) for folder in piece_folders]
-    grouped_files = [(folder, *files) for folder, *files in grouped_files if None not in files]
+    grouped_files = [(folder, *(find_ext(folder, ext) for ext in ('.mscz', '.mid', '.wav')))
+                     for folder in piece_folders]
     return grouped_files
 
 
 # Switch between discovery modes
-discover_files= discover_files_by_piece
+discover_files = discover_files_by_piece
 # discover_files= discover_files_by_type
 
 
@@ -72,12 +73,15 @@ def task_generator():
     paths = discover_files()
     os.makedirs(working_folder, exist_ok=True)
     for (piece_id, ref_path, perf_path, audio_path) in paths:
-        yield from get_tension.gen_tasks(piece_id, ref_path, perf_path, working_folder=working_folder)
-        yield from get_loudness.gen_tasks(piece_id, audio_path, working_folder=working_folder)
-        yield from get_beats.gen_tasks(piece_id, ref_path, perf_path, working_folder=working_folder)
-        yield from get_alignment.gen_tasks(piece_id, ref_path, perf_path, working_folder=working_folder)
-        yield from get_sustain.gen_tasks(piece_id, perf_path, working_folder=working_folder)
-        yield from get_onset_velocity.gen_tasks(piece_id, perf_path, working_folder=working_folder)
+        if audio_path:
+            yield from get_loudness.gen_tasks(piece_id, audio_path, working_folder=working_folder)
+        if perf_path:
+            yield from get_onset_velocity.gen_tasks(piece_id, perf_path, working_folder=working_folder)
+            yield from get_sustain.gen_tasks(piece_id, perf_path, working_folder=working_folder)
+            if ref_path:
+                yield from get_tension.gen_tasks(piece_id, ref_path, perf_path, working_folder=working_folder)
+                yield from get_beats.gen_tasks(piece_id, ref_path, perf_path, working_folder=working_folder)
+                yield from get_alignment.gen_tasks(piece_id, ref_path, perf_path, working_folder=working_folder)
 
 
 if __name__ == "__main__":
@@ -88,6 +92,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.ref and args.perf:
-        globals['discover_files'] = lambda:[(args.ref, args.perf)]
+        globals['discover_files'] = lambda: [(args.ref, args.perf)]
     run_doit(globals)
-    
